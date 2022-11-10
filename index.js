f4b48aaf69e2b4093009e1b4acf2c128bafbbd73
sushmitha <sushmitha.krosuri11@gmail.com>
2022-11-10 13:10:20 +0000

    express intro program

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Hello Sushmitha");
+});
+
+app.get("/date", (request, response) => {
+  let date = new Date();
+  response.send(`Todays date is ${date}`);
+});
+
+app.get("/page", (request, response) => {
+  response.sendFile("./page.html", { root: __dirname });
+});
+app.listen(3000);
